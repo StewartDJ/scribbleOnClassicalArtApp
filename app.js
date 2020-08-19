f2e535eac86434d386207af284fbc8ed057b5124
DJ Stewart <deshastewart1992@gmail.com>
2020-08-19 11:42:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', () => {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+//resizing
+
+canvas.height = window.innerHeight; 
+canvas.width = window.innerWidth;
+
+//drawing
+// ctx.beginPath();
+// ctx.moveTo(100,100);
+// ctx.lineTo(200, 100);
+// ctx.lineTo(200, 150);
+// ctx.closePath();
+// ctx.stroke();
+
+
+//variables
+let painting = false; 
+function startPosition(e){
+    painting = true;
+    draw(e);
+
+}
+
+function finishedPosition(){
+    painting = false;
+    ctx.beginPath();
+
+}
+
+function draw(e) {
+    if(!painting) return;
+    ctx.lineWidth = 10;
+    ctx. lineCap = 'round';
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+    
+}
+
+
+//event listeners
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', finishedPosition);
+canvas.addEventListener('mousemove', draw);
+
+
+
+ctx.font = 'italic 70px impact';
+    ctx.textAlign = 'left';
+    ctx. textBaseline = 'middle';
+    ctx.fillStyle = 'white';  
+    ctx.fillText('Improve classical art by scribbling all over it! Try it with your mouse!', 300, 100); 
+
+});
+
+window.addEventListener('resize')
+
